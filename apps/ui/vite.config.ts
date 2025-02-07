9cca725b8ff930115d9e93e852d50f4490f26dee
Martin Miranda <martinmirandac@gmail.com>
2025-02-07 11:50:59 +1100

    testing setup

--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -34,6 +34,7 @@ export default defineConfig({
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
+    setupFiles: ['./vitest-setup.ts'],
     coverage: {
       reportsDirectory: '../../coverage/apps/ui',
       provider: 'v8',
